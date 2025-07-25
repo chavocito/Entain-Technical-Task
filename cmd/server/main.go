6f3d574a130c13242fbd4719d005b6798b4fb534
chavocito <francbota94@gmail.com>
2025-07-25 18:23:30 +0000

    added main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/chavocito/entain/internal/db"
+	handler "github.com/chavocito/entain/internal/handler"
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Go time...")
+	connectionPool, err := db.ConnectDB()
+	if err != nil {
+		log.Fatal("Cannot connect to databse")
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Println("Server is up and running") })
+	r.HandleFunc("/user/{userId}/transaction", handler.UserTransactionHandler(connectionPool)).Methods("POST")
+	r.HandleFunc("/user/{userId}/balance", handler.GetUserBalanceHandler(connectionPool)).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
 }
